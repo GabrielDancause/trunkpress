5af4f2c02bae33ab538ca04bf2a94e6f6e90b508
Gabriel Dancause <gab@Gabriels-MacBook-Pro.local>
2026-03-26 11:10:20 +0700

    feat: add @astrojs/sitemap for proper sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://trunkpress.photonbuilder.com',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
